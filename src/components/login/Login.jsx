72de4ece3ee915293def3318b526e1b54f4bcd9a
accofankush <emailfankush@gmail.com>
2022-05-20 09:48:30 +0530

    Login Component Complete

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,20 +1,74 @@
-import React from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import Joi from "joi";
+import { http } from "../../config/axiosConfig";
+
+const loginSchema = Joi.object({
+  email: Joi.string()
+    .min(4)
+    .max(50)
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required()
+    .label("Email"),
+  password: Joi.string().min(4).max(50).required().label("Password"),
+});
 
 function Login() {
+  const [formData, setFormData] = useState({});
+  const [errors, setErrors] = useState([]);
+
+  const handleFormChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    //validate formdata
+    let result = loginSchema.validate(formData, { abortEarly: false });
+    if (result.error) {
+      setErrors(result.error.details);
+      return;
+    }
+    http
+      .post("auth", formData)
+      .then(res => {
+        if (res.status === 200) {
+          setErrors([]);
+        }
+      })
+      .catch(err =>
+        setErrors([{ message: err.response.data, path: ["server"] }]),
+      );
+
+    //send login request back
+  };
+  console.log(errors);
+
   return (
-    <Form>
+    <Form onChange={handleFormChange} onSubmit={handleSubmit}>
+      {errors?.find(er => er?.path[0] == "server")?.message && (
+        <Alert variant="danger">
+          {errors?.find(er => er?.path[0] == "server")?.message}
+        </Alert>
+      )}
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control name="email" type="email" placeholder="Enter email" />
-        <Form.Text className="text-danger">
-          We'll never share your email with anyone else.
-        </Form.Text>
+        {errors && (
+          <Form.Text className="text-danger">
+            {errors?.find(er => er?.path[0] == "email")?.message}
+          </Form.Text>
+        )}
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control name="password" type="password" placeholder="Password" />
+        {errors && (
+          <Form.Text className="text-danger">
+            {errors?.find(er => er?.path[0] == "password")?.message}
+          </Form.Text>
+        )}
       </Form.Group>
 
       <Button variant="primary" type="submit">
